8f403ec90b1d07a69a5e8629def1e7367dc228ea
Josh Cooper <cooper.s.josh@outlook.com>
2018-10-23 09:07:56 -0700

    Implements much of exceptions.h

--- include/internals/exceptions.h
+++ include/internals/exceptions.h
@@ -2,13 +2,32 @@
 
 namespace CherylE
 {
-    class base_exception : std::exception
-    {
-    private:
-        //todo: keep these?
-        const char* msg;
+    class base_exception : public std::exception{
+    protected:
+        char msg[2048];
     public:
-        //todo: generate stack trace
-        base_exception(const char*);
+        base_exception(const char* function, uint32_t line, const char* info){
+            snprintf(msg,2048,"[%s:%d]\n",function,line);
+            size_t l = strlen(msg);
+            strncpy(msg+l,info,2048-l-1);
+        }
+
+        virtual const char* what() const {
+            return msg;
+        }
     };
+
+    class bad_alloc : public base_exception{
+    public:
+        char stacktrace[4096];
+        bad_alloc(const char* location_, uint32_t line_)
+         :base_exception(location_, line_,"bad allocation"){
+            //todo: fill stacktrace
+        }
+    };
+}
+
+std::ostream& operator<<(std::ostream &os, CherylE::base_exception &obj){
+    os << obj.what();
+    return os;
 }
