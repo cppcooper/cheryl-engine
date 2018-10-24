cb1a29619a9c5c1bd7f239aeba0db147dd0f6160
Josh Cooper <cooper.s.josh@outlook.com>
2018-10-23 19:10:04 -0700

    Updates internals.h includes

--- include/internals.h
+++ include/internals.h
@@ -2,7 +2,9 @@
 #ifndef INTERNALS_H
 #define INTERNALS_H
 
+#include "internals/posh.h"
 #include "internals/exceptions.h"
+#include "internals/stacktrace.h"
 #include "internals/singleton.h"
 #include "internals/macros.h"
 
