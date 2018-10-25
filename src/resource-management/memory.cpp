9c70a7eed285afedf558ae13d46da5ae29294ef5
Josh Cooper <cooper.s.josh@outlook.com>
2018-10-24 17:10:18 -0700

    Implements MemoryMgr::get(bytes)

--- src/resource-management/memory.cpp
+++ src/resource-management/memory.cpp
@@ -1,15 +1,33 @@
 #include <resource-management/memory.h>
-#include <cstdlib>
 using namespace CherylE;
 
 void* MemoryMgr::get(size_t bytes) {
-    //todo: check for available allocation
-    void* ptr = malloc(bytes);
-    if (ptr == nullptr) {
-        throw base_exception("replace with bad_alloc");
+    uint32_t remainder = 0;
+    uint64_t ptr = nullptr;
+    auto iter = OpenList.lower_bound(bytes);
+    if(iter != OpenList.end()){
+        remainder = iter->first - bytes;
+        auto ptr_pair = iter->second;
+        ptr = ptr_pair.second;
+        OpenList.erase(iter);
+        if(remainder>0){
+            OpenList.emplace(remainder,ptr+bytes);
+        }
+        ClosedList.emplace(ptr_pair.first,std::make_pair(ptr,bytes));
     }
-    //todo: stash allocation remainder
-    return ptr;
+    else{
+        remainder = bytes >= default_alloc_size ? 0 : default_alloc_size - bytes;
+        ptr = malloc(remainder != 0 ? default_alloc_size : bytes);
+        if (!ptr) {
+            throw bad_alloc(__FUNCTION__,__LINE__);
+        }
+        MasterRecord.emplace(ptr);
+        if(remainder>0){
+            OpenList.emplace(remainder,ptr+bytes);
+        }
+        ClosedList.emplace(ptr,std::make_pair(ptr,bytes));
+    }
+    return (void*)ptr;
 }
 void MemoryMgr::put(void *ptr, size_t bytes) {
 
