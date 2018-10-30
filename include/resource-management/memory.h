258d2c8a5becf52a4f7a91dc18e435f8f265afc3
Josh Cooper <cooper.s.josh@outlook.com>
2018-10-30 09:16:40 -0700

    Revises memory.h
    -Removes set_default_alloc_size method
    -Changes ptr type
    -Changes memory size type to size_t
    -Changes alloc type to that of a struct
    -Replaces two methods
    -Adds new methods
    -Adds new members: free/used/total
    -Adds pre_allocate method
    -Adds fitType enum
     -Revises `get()` signature to take a fitType

--- include/resource-management/memory.h
+++ include/resource-management/memory.h
@@ -11,22 +11,45 @@ namespace CherylE
     {
         TYPENAMEAVAILABLE_STATIC
     private:
-        using ptr = uint64_t;
+        using ptr = void*;
         using masterptr = ptr;
-        using alloc = std::pair<ptr,uint32_t>;
-        uint32_t default_alloc_size;
+        struct alloc{
+            ptr master;
+            ptr head;
+            size_t master_size;
+            size_t head_size;
+        };
+        size_t m_free;
+        size_t m_used;
+        size_t m_total;
         //tracks allocations to prevent memory leaks
         std::unordered_set<masterptr> MasterRecord;
         //lookup table for available allocations
-        std::multimap<uint32_t,std::pair<masterptr,ptr>> OpenList;
+        std::multimap<size_t,alloc> OpenList;
         //lookup table for sub-allocations
-        std::unordered_multimap<masterptr,alloc> ClosedList;
+        std::multimap<masterptr,alloc> ClosedList;
+
+    protected:
+        alloc allocate(size_t &bytes);
+        using closed_iter = std::multimap<masterptr,alloc>::iterator;
+        void open_alloc(closed_iter iter, size_t bytes);
+        closed_iter find(void* p);
 
     public:
-        void* get(size_t bytes);
-        void put(void* ptr, size_t bytes);
-        uint64_t length();
-        uint64_t size();
+        void pre_allocate(size_t bytes, size_t blocks = 1);
+        void* get(size_t bytes, fitType fit = fitType::bestFit);
+        size_t size(void* p);
+        bool resize(void* p, size_t bytes, bool allow_realloc = false);
+        void put(void* p);
+        void put(void* p, size_t bytes);
+        //void update/cleanup --needs time data
+        size_t free()const{ return m_free; };
+        size_t used()const{ return m_used; };
+        size_t total()const{ return m_total; };
+        enum fitType{
+            bestFit,
+            worstFit
+        };
     };
 
     template<class T>
