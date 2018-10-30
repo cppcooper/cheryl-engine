396dc92d0522320691684d60b33defaaa16abba3
Josh Cooper <cooper.s.josh@outlook.com>
2018-10-30 09:12:43 -0700

    Updates memory.h
    -Adds data structures for tracking allocations
    -Adds TypeName method to MemoryMgr
    -Completes implementation of Allocator<T>

--- include/resource-management/memory.h
+++ include/resource-management/memory.h
@@ -9,8 +9,19 @@ namespace CherylE
 {
     class MemoryMgr
     {
+        TYPENAMEAVAILABLE_STATIC
     private:
+        using ptr = uint64_t;
+        using masterptr = ptr;
+        using alloc = std::pair<ptr,uint32_t>;
         uint32_t default_alloc_size;
+        //tracks allocations to prevent memory leaks
+        std::unordered_set<masterptr> MasterRecord;
+        //lookup table for available allocations
+        std::multimap<uint32_t,std::pair<masterptr,ptr>> OpenList;
+        //lookup table for sub-allocations
+        std::unordered_multimap<masterptr,alloc> ClosedList;
+
     public:
         void* get(size_t bytes);
         void put(void* ptr, size_t bytes);
@@ -22,8 +33,11 @@ namespace CherylE
     class Allocator : public iAllocator
     {
     public:
-        void* get(size_t count) override {
-            Singleton<MemoryMgr>::getInstance().get(count * sizeof(T));
+        void *allocate(size_t count) override {
+            return Singleton<MemoryMgr>::getInstance().get(sizeof(T) * count);
+        }
+        void deallocate(void *ptr, size_t count) override {
+            Singleton<MemoryMgr>::getInstance().put(ptr, sizeof(T) * count);
         }
     };
 }
