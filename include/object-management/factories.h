dbda4e8e5cfe5224f79ad6d03ae1c7ab5a9c60d8
Josh Cooper <cooper.s.josh@outlook.com>
2018-11-04 00:44:10 -0700

    Implements AssetFactory with iFactory

--- include/object-management/factories.h
+++ include/object-management/factories.h
@@ -1,11 +1,31 @@
 #pragma once
-
+#include "../internals.h"
 #include "../../../tools_logger.h"
 #include "../STL.h"
 #include "../InterAccess.h"
 #include "../AssetAbstract.h"
 #include "AssetMgr.h"
 #include "Pool.h"
+
+
+namespace CherylE{    
+    template<class AssetType>
+    class AssetFactory : public iFactory<iAsset,AssetFactory>{
+        iAsset* create(){
+            AssetType* p2 = AssetPool<AssetType>.get(1);
+            return static_cast<iAsset*>(p2);
+        }
+        void destroy(iAsset* p){
+            AssetType* p2 = static_cast<AssetType*>(p);
+            AssetPool<T>.put(p2);
+        }
+        template<typename... Args>
+        void construct_impl(iAsset* p, Args... args){
+            AssetType* p2 = static_cast<AssetType*>(p);
+            *p2 = AssetType{std::forward<Args>(args)...};
+        }
+    };
+}
 
 //todo: replace ManagedObject with AssetObject
 //************
