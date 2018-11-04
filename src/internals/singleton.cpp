7d74c31b158b677a1ebd50c81c8b339a5566acbd
Josh Cooper <cooper.s.josh@outlook.com>
2018-11-04 00:17:11 -0700

    Removes obsolete singleton.cpp

--- src/internals/singleton.cpp
+++
@@ -1,8 +0,0 @@
-#include "singleton.h"
-#include <iostream>
-
-namespace detail{
-    void print_already_constructed_error(const char* type_name){
-        std::cerr << "Already constructed Singleton<" << type_name << ">\n";
-    }
-}
