ca78a4f00043202c0864e0aff7a2bef2190f2bae
Josh Cooper <cooper.s.josh@outlook.com>
2018-11-04 01:09:24 -0700

    Adds incomplete GenericFactory

--- include/object-management/factories.h
+++ include/object-management/factories.h
@@ -4,6 +4,7 @@
 namespace CherylE{    
     template<class AssetType>
     class AssetFactory : public iFactory<iAsset,AssetFactory>{
+    public:
         iAsset* create(){
             AssetType* p2 = AssetPool<AssetType>.get(1);
             return static_cast<iAsset*>(p2);
@@ -16,6 +17,31 @@ namespace CherylE{
         void construct_impl(iAsset* p, Args... args){
             AssetType* p2 = static_cast<AssetType*>(p);
             *p2 = AssetType{std::forward<Args>(args)...};
+        }
+    };
+
+    template<class T>
+    class GenericFactory : public iFactory<void,GenericFactory>{
+    public:
+        static_assert(hasmethod(T,TypeName),"GenericFactory<T> requires T to implement the TypeName method to provide type safety.");
+        static_assert(!isderived(iAsset,T),"Use AssetFactory for iAsset derived types.");
+        //todo: implement methods
+        void* create(){
+            T* p = nullptr;
+            return (void*)p;
+        }
+        void destroy(void* p){
+            T* p2 = (T*)p;
+            //if p2->TypeName() == validT.TypeName()
+            //else throw bad_request exception
+        }
+        template<typename... Args>
+        void construct_impl(void* p, Args... args){
+            static T validT;
+            T* p2 = (T*)p;
+            //if p2->TypeName() == validT.TypeName()
+            *p2 = T{std::forward<Args>(args)...};
+            //else throw bad_request exception
         }
     };
 }
