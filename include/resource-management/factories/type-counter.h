df6eefd9c890a5010764615c56777084222017d1
Josh Cooper <cooper.s.josh@outlook.com>
2018-12-15 23:02:15 -0800

    Implements basic factory

--- include/resource-management/factories/type-counter.h
+++
@@ -1,9 +0,0 @@
-namespace CherylE{
-    size_t typeCounter(bool count = false){
-        static size_t count = 0;
-        if(count){
-            return ++count;
-        }
-        return count;
-    }
-}
