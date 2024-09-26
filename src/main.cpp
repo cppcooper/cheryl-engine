ddd2cd2b2e59803f1c3c4b155927b923f3df339c
Josh Cooper <cooper.s.josh@outlook.com>
2024-09-26 12:01:06 -0700

    Flattened Git History Demarcation Point (Adds main.cpp)

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#ifdef NDEBUG
+  #include <internals/celog.h>
+#else
+  #define ST_ON_SIGNALS
+  #include <internals.h>
+  backward::SignalHandling sh;
+#endif
+//#include <cheryl.h>
+backward::TraceResolver tr;
+#include <resources.h>
+#include <engine.h>
+
