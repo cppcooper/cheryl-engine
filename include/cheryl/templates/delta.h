70dc26d102d37292061cbe08196668a3f066cc9b
Josh Cooper <cooper.s.josh@outlook.com>
2024-09-25 19:25:42 -0700

    Adds DeltaTime template

---
+++ include/cheryl/templates/delta.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <chrono>
+
+template<typename ClockType = std::chrono::steady_clock>
+class DeltaTime {
+public:
+    using TimePoint = typename ClockType::time_point;
+    using Duration  = typename ClockType::duration;
+
+protected:
+    TimePoint last_checkin;
+
+    void checkin() { last_checkin = ClockType::now(); }
+    Duration elapsed() const { return ClockType::now() - last_checkin; }
+
+public:
+    double operator()() {
+        double delta = std::chrono::duration<double>(elapsed()).count();
+        checkin();
+        return delta;
+    }
+};
