845c875f174e16a904ba14b9e82c402e5e1d0b71
Josh Cooper <cooper.s.josh@outlook.com>
2024-11-11 21:40:29 -0400

    Adds templates/observed-variables.h

---
+++ include/cheryl/templates/observed-variables.h
@@ -0,0 +1,55 @@
+#pragma once
+#ifndef OBSERVED_VARIABLES_H
+#define OBSERVED_VARIABLES_H
+#include <atomic>
+#include <mutex>
+#include <shared_mutex>
+#include <condition_variable>
+
+template<typename T, uint8_t Observers = 1>
+struct ObservedVariable {
+    static_assert(Observers != 0, "The number of observers is not allowed to be less than 1.");
+    using Callback = void(*)(const T&);
+    explicit ObservedVariable(T v, std::array<Callback, Observers> callbacks) : var(std::move(v)), callbacks(std::move(callbacks)) {}
+
+    ObservedVariable& operator=(T v) { set(v); return *this; }
+    void set(T v) {
+        std::unique_lock wl(mtx);
+        if (var != v) {
+            var = std::move(v);
+            cv.notify_all();
+        }
+        wl.release();
+        std::shared_lock rl(mtx);
+        for(auto callback : callbacks) {
+            if (callback) {
+                callback(var);
+            }
+        }
+    }
+    [[nodiscard]] T get() const {
+        std::shared_lock rl(mtx);
+        return var;
+    }
+    // I am not sure I understand how to use the predicate form of wait correctly
+    void wait_until_change() {
+        std::shared_lock lock(mtx);
+        auto ov = q;
+        while(true) {
+            cv.wait(lock);
+            if (ov != q) break;
+        }
+    }
+protected:
+    uint64_t q = 0;
+    T var;
+    std::shared_mutex mtx;
+    std::condition_variable_any cv;
+    std::array<Callback, Observers> callbacks;
+};
+
+inline void foo() {
+    ObservedVariable A(0,{[](const int&){}});
+}
+
+#endif //OBSERVED_VARIABLES_H
