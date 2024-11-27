ec6f0eece8a3b875b1f2ea3d4fc12488ea807897
Josh Cooper <cooper.s.josh@outlook.com>
2024-11-27 13:10:52 -0400

    Adds documentation for event system

--- include/cheryl/core/subsystems/event-system.h
+++ include/cheryl/core/subsystems/event-system.h
@@ -7,6 +7,18 @@
 #include <shared_mutex>
 #include <templates/singleton.h>
 
+/* Event System
+ * Modeled as a publisher/subscriber pattern, allowing immediate notification to listeners.
+ * Allows runtime event creation, in order to provide a light-weight extendable event system with very little overhead.
+ * Utilizes std::any to deliver payloads of any form and size.
+ *
+ * Warning: this all means that dispatching an event with or without a specific payload has the potential
+ * to break a listener's expectations and consequently blow up.
+ *
+ * So it is up to the listener to compensate for this risk.
+ * Or on the developer to fucking write consistent [safe] event code.
+ */
+
 namespace CE::SubSystems {
     struct EventSystem : Singleton_CTS<EventSystem> {
         using Callback = std::function<void(std::any)>;
