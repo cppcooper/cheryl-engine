a5ecdeed808e8f011c94df1647816b9683d9202e
Josh Cooper <cooper.s.josh@outlook.com>
2024-11-29 15:19:51 -0400

    Introduces comments about the refactor goals/decisions

--- include/cheryl/core/engines/opengl-engine.h
+++ include/cheryl/core/engines/opengl-engine.h
@@ -10,10 +10,32 @@
 
 #include "abstract-engine.h"
 
+/* Questions about the division between systems and their uses:
+ *  * Camera
+ *  * glEngine => glWrapper?
+ *  * DisplaySystem
+ *  * Renderer
+ *  Camera will need information from DisplaySystem, which it can just cache from events
+ *
+ * glEngine definitely needs to become a proper Facade(pg 185)
+ * likely to rename it and the base class (engine -> wrapper)
+ *
+ * ChatGPT recommended a Renderer interface, thereby meaning we'll implement an OpenGLRenderer to actualize it
+ * and later a VulkanRenderer and DirectX renderer or whatever feels right and has time made for it
+ *
+ * Additionally, a Factory and enum should be implemented to instantiate the user's choice. Thus allowing them to
+ * remain unaware of the actual class name if they choose.
+ *
+ * Along with these decisions, other things like shaders and textures may call for the creation of
+ * abstractions/interfaces for these ideas, and implementations which call upon the correct backend.
+ * i.e. a Shader abstraction/interface, GLSLProgram implementation integrated with OpenGL (opengl -> GLSL)s
+ */
+
 namespace CE::Engine {
     struct glEngine final : iEngine {
     private:
         DisplaySystem display;
+        // todo: move everything to a camera object
         ObservedVariable<Enum::gfx_mode> m_gMode;
         ObservedVariable<float> m_nearplane;
         ObservedVariable<float> m_farplane;
