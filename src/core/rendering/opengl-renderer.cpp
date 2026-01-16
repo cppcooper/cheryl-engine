79ff18b3e22e5adfdf5ed1a34ee575b254dd9efa
Joshua Cooper <cooper.s.josh@outlook.com>
2026-01-16 11:54:00 -0800

    Replaces std::round with std::lround calls in opengl-renderer.cpp

--- src/core/rendering/opengl-renderer.cpp
+++ src/core/rendering/opengl-renderer.cpp
@@ -18,8 +18,8 @@ namespace CE::RenderAPIs{
             const auto pm = display->primary_monitor;
             float sw, sh;
             glfwGetMonitorContentScale(pm.glfw_monitor, &sw, &sh);
-            const uint16_t width = round(pm.width / sw);
-            const uint16_t height = round(pm.height / sh);
+            const uint16_t width = std::lround(pm.width / sw);
+            const uint16_t height = std::lround(pm.height / sh);
             display->create_window(pm, Enum::window_mode::NORMAL, width, height)->activate();
             glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
             glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
