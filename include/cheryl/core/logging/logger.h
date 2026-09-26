71a262dc6591a98ec3d224f0b707d9ecfa5f8f50
Joshua Cooper <cooper.s.josh@outlook.com>
2026-09-25 18:16:38 -0700

    Updates logger wrapper lifecycle interface
    - Propagates close timeouts through Logger<name>
    - Preserves existing zero-argument close calls
    - Adds explicit standard-library dependencies for wrapper usage

--- include/cheryl/core/logging/logger.h
+++ include/cheryl/core/logging/logger.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "log.h"
 #include <templates/singleton.h>
-#include <string>
+
+#include <chrono>
+#include <utility>
 
 namespace CE {
-    template<const char* name>
+    template <const char* name>
     class Logger : public Singleton_CTS<Log<name>> {
     protected:
         explicit Logger(const spdlog::file_event_handlers& event_handlers = {}) {
@@ -12,51 +14,69 @@ namespace CE {
         }
 
     public:
-        static void set_pattern(const char* fmt) { Singleton_CTS<Log<name>>::get().set_pattern(fmt); }
-        [[nodiscard]]
-        static std::filesystem::path get_file_path() { return Singleton_CTS<Log<name>>::get().get_file_path(); };
+        static void set_pattern(const char* fmt) {
+            Singleton_CTS<Log<name>>::get().set_pattern(fmt);
+        }
 
-        static void flush() { Singleton_CTS<Log<name>>::get().flush(); }
+        [[nodiscard]] static std::filesystem::path get_file_path() {
+            return Singleton_CTS<Log<name>>::get().get_file_path();
+        }
 
-        static void close() { Singleton_CTS<Log<name>>::get().close(); }
+        static void flush() {
+            Singleton_CTS<Log<name>>::get().flush();
+        }
 
-        static void reopen() { Singleton_CTS<Log<name>>::get().reopen(); }
+        static void close(std::chrono::milliseconds timeout = std::chrono::milliseconds::zero()) {
+            Singleton_CTS<Log<name>>::get().close(timeout);
+        }
 
-        static void make_default() { Singleton_CTS<Log<name>>::get().make_default(); }
+        static void reopen() {
+            Singleton_CTS<Log<name>>::get().reopen();
+        }
 
-        static void set_level_logger(spdlog::level level) { Singleton_CTS<Log<name>>::get().set_level_logger(level); }
+        static void make_default() {
+            Singleton_CTS<Log<name>>::get().make_default();
+        }
 
-        static void set_level_filesink(spdlog::level level) { Singleton_CTS<Log<name>>::get().set_level_filesink(level); }
+        static void set_level_logger(spdlog::level level) {
+            Singleton_CTS<Log<name>>::get().set_level_logger(level);
+        }
 
-        static void set_level_stdsink(spdlog::level level) { Singleton_CTS<Log<name>>::get().set_level_stdsink(level); }
+        static void set_level_filesink(spdlog::level level) {
+            Singleton_CTS<Log<name>>::get().set_level_filesink(level);
+        }
 
-        template<typename... Args>
-        static void trace(spdlog::format_string_t<Args...> fmt, Args&& ...args) {
+        static void set_level_stdsink(spdlog::level level) {
+            Singleton_CTS<Log<name>>::get().set_level_stdsink(level);
+        }
+
+        template <typename... Args>
+        static void trace(spdlog::format_string_t<Args...> fmt, Args&&... args) {
             Singleton_CTS<Log<name>>::get().trace(fmt, std::forward<Args>(args)...);
         }
 
-        template<typename... Args>
-        static void debug(spdlog::format_string_t<Args...> fmt, Args&& ...args) {
+        template <typename... Args>
+        static void debug(spdlog::format_string_t<Args...> fmt, Args&&... args) {
             Singleton_CTS<Log<name>>::get().debug(fmt, std::forward<Args>(args)...);
         }
 
-        template<typename... Args>
-        static void info(spdlog::format_string_t<Args...> fmt, Args&& ...args) {
+        template <typename... Args>
+        static void info(spdlog::format_string_t<Args...> fmt, Args&&... args) {
             Singleton_CTS<Log<name>>::get().info(fmt, std::forward<Args>(args)...);
         }
 
-        template<typename... Args>
-        static void warn(spdlog::format_string_t<Args...> fmt, Args&& ...args) {
+        template <typename... Args>
+        static void warn(spdlog::format_string_t<Args...> fmt, Args&&... args) {
             Singleton_CTS<Log<name>>::get().warn(fmt, std::forward<Args>(args)...);
         }
 
-        template<typename... Args>
-        static void error(spdlog::format_string_t<Args...> fmt, Args&& ...args) {
+        template <typename... Args>
+        static void error(spdlog::format_string_t<Args...> fmt, Args&&... args) {
             Singleton_CTS<Log<name>>::get().error(fmt, std::forward<Args>(args)...);
         }
 
-        template<typename... Args>
-        static void critical(spdlog::format_string_t<Args...> fmt, Args&& ...args) {
+        template <typename... Args>
+        static void critical(spdlog::format_string_t<Args...> fmt, Args&&... args) {
             Singleton_CTS<Log<name>>::get().critical(fmt, std::forward<Args>(args)...);
         }
 
@@ -65,5 +85,3 @@ namespace CE {
         }
     };
 }
-
-
